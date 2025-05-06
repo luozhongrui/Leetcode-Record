54894a7a38732a6aee1876082464eaaa009355fe
zincneo <zincneoestonia@gmail.com>
2025-05-06 21:20:44 +0800

    feat(0001): two sum

---
+++ 1.rs
@@ -0,0 +1,21 @@
+pub mod solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        use std::collections::HashMap;
+        let mut map = HashMap::<i32, i32>::new();
+        for (idx, &val) in nums.iter().enumerate() {
+            if let Some(&preidx) = map.get(&(target - val)) {
+                return vec![preidx, idx as i32];
+            } else {
+                map.insert(val, idx as i32);
+            }
+        }
+        panic!("incorrect testcase");
+    }
+}
+
+fn main() {
+    use solution::two_sum;
+    assert_eq!(vec![0, 1], two_sum(vec![2, 7, 11, 15], 9));
+    assert_eq!(vec![1, 2], two_sum(vec![3, 2, 4], 6));
+    assert_eq!(vec![0, 1], two_sum(vec![3, 3], 6));
+}
